c0637d5653c785a986a50d9b14c8fe9002a2c1a2
Jasper Oosterbroek <jasper.oosterbroek@student.hu.nl>
2019-04-19 13:52:26 +0200

    Add templating to mq_sensor class and make printing gas for debugging possible

--- code/headers/mq_sensor.hpp
+++ code/headers/mq_sensor.hpp
@@ -2,20 +2,85 @@
 #include <cmath>
 #include <hwlib.hpp>
 
-template<int number_of_gasses>
+struct gas{
+  int gas_id, value;
+
+  friend hwlib::ostream & operator<< (hwlib::ostream &out, const gas &g){
+    return out << "gas_id: " << g.gas_id << " value: " << g.value;
+  }
+
+};
+
+struct mq_sensors_gas_curve{
+  int gas_id;
+  std::array<float, 3> gas_curve = {};
+
+};
+
+
+template<int gasses_amount>
 class mq_sensor_c{
+
 private:
   hwlib::target::pin_adc & adc_pin;
   int rl; //load resistor
-  int ro;
+  int ro = 0;
   float ro_clean_air_factor;
-  number_of_gasses, std::array<float, 3> gas_curves;
-  
+  std::array<mq_sensors_gas_curve, gasses_amount> gas_curves;
+  std::array<gas, gasses_amount> gasses;
   int resistance_calculation(int raw_adc);
 
 public:
-  mq_sensor_c(hwlib::target::pin_adc & adc_pin, int rl, float ro_clean_air_factor, std::array<float, 3> curve);
+  mq_sensor_c<gasses_amount>(hwlib::target::pin_adc & adc_pin, int rl, float ro_clean_air_factor, std::array<mq_sensors_gas_curve, gasses_amount> curve);
   int calibrate(int sample_times, int interval_wait_ms);
-  int read(int sample_time, int interval_wait_ms);
+  std::array<gas, gasses_amount> read(int sample_time, int interval_wait_ms);
 
 };
+
+template<int gasses_amount>
+mq_sensor_c<gasses_amount>::mq_sensor_c(hwlib::target::pin_adc & adc_pin, int rl, float ro_clean_air_factor, std::array<mq_sensors_gas_curve, gasses_amount> gas_curves):
+  adc_pin(adc_pin),
+  rl(rl),
+  ro_clean_air_factor(ro_clean_air_factor),
+  gas_curves(gas_curves)
+{
+  for(size_t i = 0; i < gas_curves.size(); i++){
+    gasses[i].gas_id = gas_curves[i].gas_id;
+  }
+ }
+
+template<int gasses_amount>
+int mq_sensor_c<gasses_amount>::calibrate(int sample_times, int interval_wait_ms){
+  int val = 0;
+  for(int i = 0; i < sample_times; i++){
+    val += resistance_calculation(adc_pin.read());
+    hwlib::wait_ms(interval_wait_ms);
+  }
+  val = val/sample_times;
+  val = val/ro_clean_air_factor;
+  ro = val;
+  return val;
+
+};
+
+template<int gasses_amount>
+int mq_sensor_c<gasses_amount>::resistance_calculation(int raw_adc){
+  return rl*(4095-raw_adc)/raw_adc;
+};
+
+template<int gasses_amount>
+std::array<gas, gasses_amount> mq_sensor_c<gasses_amount>::read(int sample_times, int interval_wait_ms){
+  int rs = 0;
+  for(int i = 0; i < sample_times; i++)
+  {
+    int test = resistance_calculation(adc_pin.read());
+    rs += test;
+    hwlib::wait_ms(interval_wait_ms);
+  }
+
+  for(size_t i = 0; i < gas_curves.size(); i++){
+    gasses[i].value = static_cast<int>(pow(10, (((log((rs/sample_times)/ro) - gas_curves[i].gas_curve[1]) / gas_curves[i].gas_curve[2]) + gas_curves[i].gas_curve[0])));
+  }
+  
+  return gasses;
+}
--- code/main.cpp
+++ code/main.cpp
@@ -1,25 +1,49 @@
 #include <mq_sensor.hpp>
 
+template<typename T, std::size_t N>
+hwlib::ostream & operator<< (hwlib::ostream & out, const std::array<T, N> & rhs)
+{
+    for(auto & object : rhs){
+      out << object << '\n';
+    }
+    return out;
+}
+
 int main(void) {
-  // to do remove floats (possible fix is factor 1000 everything)
-
-
   // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
     auto mq_pin = hwlib::target::pin_adc(hwlib::target::ad_pins::a0);
-    int ro = 5; //found in datasheet
+    int rl = 10; //found in datasheet
     float ro_clean_air_factor = 9.83; // see datasheet air line
 
-    std::array<float, 3> gas_lpg = {2.30, 0.48, -0.37}; // somewhere in the datasheet, log table
-    mq_sensor_c mq_2(mq_pin, ro, ro_clean_air_factor, gas_lpg);
+    constexpr int co_gas_id = 1;
+    std::array<float, 3> co_gas_curve = {2.30, 0.72, -0.34};
+    mq_sensors_gas_curve co;
+    co.gas_id = co_gas_id;
+    co.gas_curve = co_gas_curve;
+
+    constexpr int lpg_gas_id = 0;
+    std::array<float, 3> gas_lpg = {2.30, 0.72, -0.34}; // somewhere in the datasheet, log table
+    mq_sensors_gas_curve lpg;
+    lpg.gas_id = lpg_gas_id;
+    lpg.gas_curve = gas_lpg;
+
+    constexpr int gasses_amount = 2;
+    
+    std::array<mq_sensors_gas_curve, gasses_amount> mq_2_gasses = {co, lpg  };
+
+    mq_sensor_c<gasses_amount> mq_2(mq_pin, rl, ro_clean_air_factor, mq_2_gasses);
     
     hwlib::cout << "start heating" << '\n';
-    hwlib::wait_ms(20'000); // the module needs to heat up!
+    //hwlib::wait_ms(20'000); // the module needs to heat up!
     hwlib::cout << "done heating" << '\n';
 
     hwlib::cout << "calibrate result: " << static_cast<int>(mq_2.calibrate(10, 500)) << '\n';
-    hwlib::cout << "read result: " << static_cast<int>(mq_2.read(50, 500)) << '\n';
+    for(;;){
+      hwlib::cout << "read result: " << mq_2.read(1, 20) << '\n';
+    }
+    
 
     
 }
