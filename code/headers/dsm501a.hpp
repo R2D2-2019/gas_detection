8d6231a2b853f0814c4dbf07e543ada8eebf15c4
vera98x <v.schoonderwoerd@outlook.com>
2019-04-23 11:39:34 +0200

    removed unused variable

--- code/headers/dsm501a.hpp
+++ code/headers/dsm501a.hpp
@@ -3,13 +3,11 @@
 
 class dsm501a_c{
     private:
-        int dust_mgm3; //mg per m3
         hwlib::target::pin_in & pin_in;  // digital pin_in
         int sampletime_ms; //miliseconds
         int waitingtime_ms; //miliseconds
     public:
         dsm501a_c(hwlib::target::pin_in & pin_in):
-            dust_mgm3(0),
             pin_in(pin_in),
             sampletime_ms(30000),
             waitingtime_ms(50)
