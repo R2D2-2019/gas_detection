125e6f7c72005eac8df0c383d9077a009c57e853
vera98x <v.schoonderwoerd@outlook.com>
2019-04-23 11:53:39 +0200

    doxygen added

--- code/headers/dsm501a.hpp
+++ code/headers/dsm501a.hpp
@@ -3,16 +3,45 @@
 
 class dsm501a_c{
     private:
+        /**
+        * @brief 
+        * Data output pin
+        */
         hwlib::target::pin_in & pin_in;  // digital pin_in
+        /**
+        * @brief 
+        * Sampletime (30 seconds as default)
+        */
         int sampletime_ms; //miliseconds
+        /**
+        * @brief 
+        * Interval while measuring (important for RTOS)
+        */
         int waitingtime_ms; //miliseconds
     public:
-        dsm501a_c(hwlib::target::pin_in & pin_in):
-            pin_in(pin_in),
-            sampletime_ms(30000),
-            waitingtime_ms(50)
-        {}
 
+        /**
+        * @brief 
+        * Constructor dsm501a_c
+        * @param    pin_in: GPIO number
+        * @param    sampletime: measuring time, default 30000 miliseconds
+        * @param    waitingtime: Interval while measuring in milliseconds (important for RTOS, default 50 milliseconds)
+        */
+        dsm501a_c(hwlib::target::pin_in & pin_in, int sampletime = 30000, int waitingtime = 50):
+            pin_in(pin_in),
+            sampletime_ms(sampletime),
+            waitingtime_ms(waitingtime)
+        {}
+        /**
+        * @brief 
+        * Reads the gasvolume, returns a value in ug/m3
+        * @details
+        * Reads the gasvolume for 30 seconds, returns an integer value in ug/m3
+        */
         int read();
+        /**
+        * @brief 
+        * Empty function, inherits from (future) superclass
+        */
         void calibrate(){}
 };
