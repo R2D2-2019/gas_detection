4a31bc1157538955623999f169dcaf70ee3395b6
vera98x <v.schoonderwoerd@outlook.com>
2019-04-23 11:35:09 +0200

    value from mg/m3 to ug/m3

--- code/src/dsm501a.cpp
+++ code/src/dsm501a.cpp
@@ -12,10 +12,9 @@ int dsm501a_c::read(){
     // floats are not accepted, so the real value has to be divided by 100
     int lowratio_round2 = ((duration_low_ms*100.0)/sampletime_ms)*100;
     
-    // floats are not accepted, so the real value has to be divided by 100
-    int concentration_mgm3_round2 = ((0.001915 * (lowratio_round2/100.0 * lowratio_round2/100.0)) + (0.09522 * lowratio_round2/100.0) - 0.04884)*100;   // https://forum.mysensors.org/topic/9497/dust-sensor-samyoung-dsm501a/7
-    hwlib::cout << "duration_low_ms: " << duration_low_ms << " lowratio: " << lowratio_round2 << "    concentration_mgm3: " << concentration_mgm3_round2 << "\n";
+    int concentration_ugm3_round2 = ((0.001915 * (lowratio_round2/100.0 * lowratio_round2/100.0)) + (0.09522 * lowratio_round2/100.0) - 0.04884)*1000;   // https://forum.mysensors.org/topic/9497/dust-sensor-samyoung-dsm501a/7
+    hwlib::cout << "duration_low_ms: " << duration_low_ms << " lowratio: " << lowratio_round2 << "    concentration_ugm3: " << concentration_mgm3_round2 << "\n";
     start_time = hwlib::now_us();
     duration_low_ms = 0;
-    return concentration_mgm3_round2;
+    return concentration_ugm3_round2;
 }
