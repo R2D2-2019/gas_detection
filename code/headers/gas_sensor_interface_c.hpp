0f4743560bd5ddccd9b8249e010a4c98e22a873f
Jaluakbar <31691920+Jaluakbar@users.noreply.github.com>
2019-06-15 23:33:55 +0200

    removing unneeded base class

--- code/headers/gas_sensor_interface_c.hpp
+++ code/headers/gas_sensor_interface_c.hpp
@@ -5,24 +5,12 @@
 namespace r2d2::gas_detection {
 
     /**
-     * Class gas_sensor_interface_base_c is needed to create an array of
-     * gas_sensor_interface_c instances. We cant do this directly because
-     * gas_sensor_interface_c is an templated class.
-     */
-    class gas_sensor_interface_base_c {
-        /**
-         * This function calibrates the sensor.
-         */
-        virtual void calibrate() = 0;
-    };
-
-    /**
      * Class gas_sensor_interface_c provides an interface of all the gas
      * sensors.
      *
      */
     template <int AmountOfGasses>
-    class gas_sensor_interface_c : public gas_sensor_interface_base_c {
+    class gas_sensor_interface_c {
     public:
         gas_sensor_interface_c() {
         }
@@ -36,6 +24,11 @@ namespace r2d2::gas_detection {
          */
         virtual std::array<r2d2::gas_detection::gas_s, AmountOfGasses>
         get() = 0;
+
+        /**
+         * This function calibrates the sensor.
+         */
+        virtual void calibrate() = 0;
     };
 
 } // namespace r2d2::gas_detection
